2c8a87ade805502ae98de0e655ce8c0714220dfb
uhef <tuomas.jarvensivu@iki.fi>
2015-05-03 22:20:37 +0300

    Define project version and name in order not to clash with dcar project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject google-api "0.2.3"
+(defproject org.clojars.uhef/google-api "0.2.3"
   :description "Google api library for clojure"
-  :url "https://github.com/dcar/google-api"
+  :url "https://github.com/uhef/google-api"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.5.6"]
                  [cheshire "4.0.3"]]
